9a9bb76cea0a3ff065ffeb864c3ccc4597fd8cf3
l0nl1f3 <l0nl1f3@gmail.com>
2017-05-04 08:53:35 +0800

    三元环计数

--- BZOJ/bzoj3498.cpp
+++ BZOJ/bzoj3498.cpp
@@ -8,25 +8,26 @@ inline void read(int&a){char c;while(!(((c=getchar())>='0')&&(c<='9')));a=c-'0';
 namespace Triple{
 	int g[N],v[E],w[E],nxt[E],ed,st[N],en[N],m,q[M],val[M],tmp[N];
 	inline void add(int x,int y){
-	  if(d[x]>d[y])swap(x,y);
-	  v[++ed]=y;nxt[ed]=g[x];g[x]=ed;
+		if(d[x]>d[y])swap(x,y);
+		v[++ed]=y;nxt[ed]=g[x];g[x]=ed;
 	}
 	void solve(){
-	  for(i=1;i<=n;i++)if(g[i]){
-	    st[i]=m+1;
-	    for(j=g[i];j;j=nxt[j])tmp[q[++m]=v[j]]=w[j];
-	    en[i]=m;
-	    sort(q+st[i],q+m+1);
-	    for(j=st[i];j<=m;j++)val[j]=tmp[q[j]];
-	  }
-	  for(i=1;i<=n;i++)for(j=g[i];j;j=nxt[j]){
-	    k=v[j],x=st[i],y=st[k];
-	    while(x<=en[i]&&y<=en[k])if(q[x]==q[y]){
-	      z=q[x];
-	      A+=max(max(a[k],a[z]),a[i]);
-	      x++,y++;
-	    }else q[x]<q[y]?x++:y++;
-	  }
+		for(i=1;i<=n;i++)if(g[i]){
+			st[i]=m+1;
+			for(j=g[i];j;j=nxt[j])tmp[q[++m]=v[j]]=w[j];
+			en[i]=m;
+			sort(q+st[i],q+m+1);
+			for(j=st[i];j<=m;j++)val[j]=tmp[q[j]];
+		}
+		for(i=1;i<=n;i++)
+			for(j=g[i];j;j=nxt[j]){
+				k=v[j],x=st[i],y=st[k];
+				while(x<=en[i]&&y<=en[k])if(q[x]==q[y]){
+				  z=q[x];
+				  A+=max(max(a[k],a[z]),a[i]);
+				  x++,y++;
+				}else q[x]<q[y]?x++:y++;
+		}
 	}
 }
 int main(){
