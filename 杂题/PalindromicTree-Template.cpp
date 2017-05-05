48a3eba439b50a33b733a03697a657346a3dba28
l0nl1f3 <l0nl1f3@gmail.com>
2017-05-05 17:13:32 +0800

    Incomplete PT

--- 杂题/PalindromicTree-Template.cpp
+++ 杂题/PalindromicTree-Template.cpp
@@ -2,17 +2,41 @@
 using namespace std;
 #define N 301234
 #define Z 26
-int n,len[N],fail[N],s[N],ch[N][Z];
+#define ll long long
+int n,cnt,sn,lst,siz[N],len[N],fail[N],s[N],ch[N][Z];
 char str[N];
+int node(int x){
+	len[cnt]=x;
+	return cnt++;
+}
+void init(){
+	node(0);
+	node(-1);
+	fail[0]=1;s[0]=26;
+}
 int trans(int x,int p){
-	for(int i=p;s[i-len[i]-1]!=s[p];)
+	while(s[p-len[x]-1]!=s[p])
+		x=fail[x];
+	return x;
 }
 int insert(int x){
-	for()
+	int i,j;
+	s[++sn]=x;
+	j=trans(lst,sn);
+	if(!ch[j][x]){
+		i=node(len[j]+2);
+		fail[i]=ch[trans(fail[j],sn)][x];
+		ch[j][x]=i;
+	}
+	siz[lst=ch[j][x]]++;
 }
 int main(){
 	int i;
 	for(scanf("%s",str+1),n=strlen(str+1),i=1;i<=n;i++)
 		insert(str[i]-'a');
+	for(i=cnt-1;i>1;i--)siz[fail[i]]+=siz[i];
+	ll ans=0;
+	for(i=2;i<=cnt;i++)ans=max(ans,(ll)len[i]*siz[i]);
+	printf("%lld\n",ans);
 }
  
