a3f0924fb592c369dbc9846d2c3784429d3bb580
l0nl1f3 <l0nl1f3@gmail.com>
2017-05-05 15:38:25 +0800

    ...

---
+++ 杂题/PalindromicTree-Template.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define N 301234
+#define Z 26
+int n,len[N],fail[N],s[N],ch[N][Z];
+char str[N];
+int trans(int x,int p){
+	for(int i=p;s[i-len[i]-1]!=s[p];)
+}
+int insert(int x){
+	for()
+}
+int main(){
+	int i;
+	for(scanf("%s",str+1),n=strlen(str+1),i=1;i<=n;i++)
+		insert(str[i]-'a');
+}
+ 
