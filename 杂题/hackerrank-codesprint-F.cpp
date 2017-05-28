16e43c9b77b2a0c8dd6162b35b930b2b65fc2c86
l0nl1f3 <l0nl1f3@gmail.com>
2017-05-28 18:51:19 +0800

    try to improve F

--- 杂题/hackerrank-codesprint-F.cpp
+++ 杂题/hackerrank-codesprint-F.cpp
@@ -16,6 +16,20 @@ int main(){
 	}
 	int dt=0,ht=0;
 	for(i=1;i<=n;i++){
+		int da=0;
+		for(int dv=1;dv<=5;dv++){
+			int ok=1;
+			for(j=0;j<l[i];j++){
+				if(a[x[i][j]][y[i][j]-dv]){
+					ok=0;break;
+				}
+				if(y[i][j]<=dv){
+					ok=0;break;
+				}
+			}
+			if(ok)da=dv;
+		} 
+		for(j=0;j<l[i];j++)y[i][j]-=da;
 		for(j=k=0;j<l[i];j++){
 			a[x[i][j]][y[i][j]+dt]=i;
 			k=max(k,y[i][j]);ht=max(ht,x[i][j]);
@@ -27,4 +41,3 @@ int main(){
 		for(j=1;j<=dt;j++)printf("%d ",a[i][j]);
 	return 0;
 }
-
