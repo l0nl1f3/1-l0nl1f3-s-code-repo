f68a774192637d8a742560321de4141999910044
l0nl1f3 <l0nl1f3@gmail.com>
2017-07-03 21:11:28 +0800

    Wrong Answer on test 15  就当我过了好不好

--- CodeForces/codeforces-715B.cpp
+++ CodeForces/codeforces-715B.cpp
@@ -10,25 +10,25 @@ template<typename T>inline bool cmax(T &a,const T &b){ return a<b?a=b,1:0;}
 
 #define N 1234
 #define M 23333
-#define inf 1011111111
-struct edge{int to,nxt,v;}e[M];int cnt,lst[N];
-int n,m,L,s,t,a[M],b[M],c[M],d[M],dis[N];
-void ins(int a,int b,int c){e[++cnt]=(edge){b,lst[a],c};lst[a]=cnt;}
-void lnk(int a,int b,int c){ins(a,b,c);ins(b,a,c);}
+#define inf 111111111111111ll
+struct edge{int to,nxt;ll v;}e[M];int cnt,lst[N];
+int n,m,L,s,t,a[M],b[M],c[M],d[M];ll dis[N];
+void ins(int a,int b,ll c){e[++cnt]=(edge){b,lst[a],c};lst[a]=cnt;}
+void lnk(int a,int b,ll c){ins(a,b,c);ins(b,a,c);}
 void me(){memset(lst,0,sizeof(lst));cnt=0;} 
-void build(int x,int v){
+void build(int x,ll v){
 	me();
 	for(int i=1;i<=m;i++)
 		lnk(a[i],b[i],c[i]?c[i]:v+(i<=x));
 }
 #include<ext/pb_ds/priority_queue.hpp>
 namespace Dijkstra{
-	#define pa pair<int,int>
+	#define pa pair<ll,int>
 	#define mpa make_pair
 	using namespace __gnu_pbds;
 	typedef __gnu_pbds::priority_queue<pa,greater<pa>,pairing_heap_tag > heap;
 	heap::point_iterator id[N];
-	int dijkstra(int s,int t){
+	ll dijkstra(int s,int t){
 	    heap q;int i,b;
 	    for(i=1;i<=n;i++)dis[i]=inf,id[i]=0;
 	    dis[s]=0;id[s]=q.push(make_pair(0,s));
@@ -56,7 +56,7 @@ int main(){
 	if(dijkstra(s,t)<L)return puts("NO"),0;
 	for(i=1;i<=m;i++)if(!c[i])lnk(a[i],b[i],1);
 	if(dijkstra(s,t)>L)return puts("NO"),0;
-	int l=0,r,R=inf-2,o,ans;
+	ll l=0,r,R=inf,o,ans;
 	while(l<=R){
 		o=(l+R)/2;
 		build(0,o);
@@ -74,7 +74,7 @@ int main(){
 	}
 	puts("YES");
 	for(i=1;i<=m;i++)
-		printf("%d %d %d\n",a[i]-1,b[i]-1,c[i]?c[i]:(i<=r?l:R));
+		printf("%d %d %lld\n",a[i]-1,b[i]-1,c[i]?c[i]:(i<=r?l:R));
 	return 0;
 }
 
