9d26b1cbde76cfcfc56dccdb850037c1c530c8af
l0nl1f3 <l0nl1f3@gmail.com>
2017-07-03 14:48:26 +0800

    shit..

--- BZOJ/bzoj2310.cpp
+++ BZOJ/bzoj2310.cpp
@@ -7,12 +7,12 @@ template<typename T>inline bool cmin(T &a,const T &b){	return a>b?a=b,1:0;}
 template<typename T>inline bool cmax(T &a,const T &b){	return a<b?a=b,1:0;}
 #define N 10
 #define M 10000
-int n,m,up,ok,sn,qn,gt,ans,inf=1e9,f[M],g[M],mth[M],s[M],st[M],v[N][N],lg[M][N],id[1<<(2*N+1)];
+int n,m,up,ok,sn,qn,gt,ans,inf=1e9,f[M],g[M],mth[M],s[M],st[M],v[233][N],lg[M][N],id[1<<(2*N+1)];
 int gb(int x,int i){return x>>(i<<1)&3;}
 void cpy(){for(int i=1;i<=qn;i++)g[i]=f[i];}
 void clr(){for(int i=1;i<=qn;i++)f[i]=-inf;}
 int main(){
-	//freopen("hj.txt","w",stdout);
+	freopen("hj.txt","w",stdout);
 	int i,j,k,p,q,x,y,z;
 	scanf("%d%d",&n,&m);			
 	for(i=1;i<=n;i++)
@@ -48,7 +48,7 @@ int main(){
 				p=g[k]+v[i][j];q=st[k];
 				x=gb(q,j-1);y=gb(q,j);
 				z=q^(x<<(j-1<<1))^(y<<(j<<1));
-				cout<<k<<","<<q<<","<<x<<","<<y<<","<<z<<","<<p<<endl;
+				cout<<k<<","<<p<<","<<q<<","<<x<<","<<y<<","<<z<<endl;
 				if(!x&&!y){
 					cmax(f[k],p-v[i][j]);
 					cmax(f[id[z^(1<<(j-1<<1))^(2<<(j<<1))]],p);
@@ -73,8 +73,6 @@ int main(){
 					else if(x==3&&y==3){if(!z)cmax(ans,p);}
 					else if(x==3) cmax(f[id[z^y<<lg[k][j]]],p);
 					else if(y==3) cmax(f[id[z^x<<lg[k][j-1]]],p);
-				
-				//for(int k=1;k<=5;k++)printf("%d ",f[k]);puts("");
 			}
 			cpy();
 		}
