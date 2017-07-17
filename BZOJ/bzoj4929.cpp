de9accad115be1ba040c24cf8303f446c9cc9fd5
l0nl1f3 <l0nl1f3@gmail.com>
2017-07-17 13:46:56 +0800

    Deal with constant factor

--- BZOJ/bzoj4929.cpp
+++ BZOJ/bzoj4929.cpp
@@ -8,13 +8,63 @@ typedef long long LL;
 
 template <typename T> inline bool cmin(T & a, const T & b) { return a > b ? a = b, 1 : 0;}
 template <typename T> inline bool cmax(T & a, const T & b) { return a < b ? a = b, 1 : 0;}
-int inp() {
-	int x = 0, f = 1;char ch;
-	for(ch = getchar(); !isdigit(ch); ch = getchar())
-		if(ch == '-') f = -1;
-	for(; isdigit(ch); x = x * 10 + ch - '0', ch = getchar());
-	return x * f;
+
+namespace io
+{
+	const int MAXBUF = 1 << 20;
+	const int OUTPUT = 1 << 23;
+	char B[MAXBUF], *S = B, *T = B;
+	#define getc() (S == T && (T = (S = B) + fread(B, 1, MAXBUF, stdin), S == T) ? 0 : *S++)
+	#define RG register
+	template<class Type> inline Type read()
+	{
+		RG Type aa = 0; RG bool bb = 0; RG char ch, *S = io::S, *T = io::T;
+		for(ch = getc(); (ch < '0' || ch > '9') && ch != '-'; ch = getc())
+			;
+		for(ch == '-' ? bb = 1 : aa = ch - '0', ch = getc(); '0' <= ch && ch <= '9'; ch = getc())
+			aa = aa * 10 + ch - '0';
+		io::S = S, io::T = T; return bb ? -aa : aa;
+	}
+	int (*inp)() = read<int>;
+	inline int gets(RG char *s)
+	{
+		RG char *iter = s, *S = io::S, *T = io::T;
+		while(*iter = getc(), *iter == ' ' || *iter == '\n' || *iter == '\r')
+			;
+		while(*++iter = getc(), *iter != ' ' && *iter != '\n' && *iter != '\r')
+			;
+		*iter = 0;		
+		io::S = S, io::T = T;
+		return iter - s;
+	}
+	inline char get()
+	{
+		RG char ch;
+		while(ch = getc(), ch == ' ' || ch == '\n' || ch == '\r')
+			;
+		return ch;
+	}
+	
+	char buff[OUTPUT], *iter = buff;
+	template<class T>inline void P(RG T x, RG char ch = '\n')
+	{
+///*lgg*/cout << x << ch; return;
+		static int stack[110]; RG int O = 0; RG char *iter = io::iter;
+		if(!x)*iter++ = '0';
+		else
+		{
+			(x < 0) ? x = -x, *iter++ = '-' : 1;
+			for(; x; x /= 10) stack[++O] = x % 10;
+			for(; O; *iter++ = '0' + stack[O--])
+				;
+		}
+		*iter++ = ch, io::iter = iter;
+	}
+
+	inline void puts(RG const char *s) {while(*s) *iter++ = *s++;}
+	inline void output() {fwrite(buff, 1, iter - buff, stdout), iter = buff;}
 }
+using io::inp;
 struct comp{ 
 	double r, i;
 	comp() {
@@ -31,14 +81,14 @@ comp mul(comp a, comp b) { return comp(a.r * b.r - a.i * b.i, a.r * b.i + a.i * 
 comp div(comp a, double b){	return comp(a.r/b, a.i/b); }
 comp conj(comp x) { return comp(x.r, -x.i); }
 	
-comp eps[MAXN], ieps[MAXN];
+comp w[MAXN], ieps[MAXN];
 void init() {
-	double pi = acos(-1);
+	double pi = acos(-1); int bn = N >> 1;
 	for(int i = 0; i < N; i++)
-		eps[i] = comp(cos(2 * pi * i /N), sin(2 * pi * i /N));
+		w[i] = comp(cos(2 * pi * i /N), sin(2 * pi * i /N));		
 }
 
-inline void trans(int n, comp x[], comp w[]) {
+inline void trans(int n, comp x[]) {
     for (int i = 0, j = 0; i != n; i++) {
         if (i < j) std::swap(x[i], x[j]);
         for (int l = n >> 1; (j ^= l) < l; l >>= 1);
@@ -55,11 +105,11 @@ inline void trans(int n, comp x[], comp w[]) {
 }
 
 inline void dft(int n, comp x[]) {
-    trans(n, x, eps);
+    trans(n, x);
 }
 inline void idft(int n, comp x[]) {
-    trans(n, x, eps);
     reverse(x + 1, x + n);
+    trans(n, x);
     for (int i = 0; i < n; i++) x[i] = div(x[i], n);
 }
 void conv(int na, int a[], int nb, int b[], int nc, int c[]) {
@@ -134,8 +184,8 @@ int solveb0(){
 	memset(z, 0, sizeof(z));
 	conv(n + 1, x, n + n, y, n + n, z);
 	
-	for(i = 0; i < n; i++) printf("%d\n", mul(fexp(c, (LL)i * i), z[i + n]));
-	
+	for(i = 0; i < n; i++) io :: P(mul(fexp(c, (LL)i * i), z[i + n]), '\n');
+	io::output();
 	return 0;
 }
 
@@ -143,11 +193,12 @@ int solvec0(){
 	int ans = 0, i;
 	for(i = 0; i < n; i++)
 		ans = add(ans, mul(fexp(b + d + e, i), a[i]));
-	printf("%d\n", ans);
+	io :: P(ans, '\n');
 	for(i = ans = 0; i < n; i++)
 		ans = add(ans, mul(fexp(e, i), a[i]));
 	for(i = 1; i < n; i++)
-		printf("%d\n", ans);
+		io:: P(ans, '\n');
+	io::output();
 	return 0;
 }
 
@@ -169,29 +220,40 @@ int main() {
 	
 	int o = mul(d, fexp(b + b, P - 2)), q = mul(e, fexp(b, P - 2));
 	q = sub(q, mul(o, o));
+	int fq = 1, fb = 1;
 	for(i = 0; i < n; i++){
-		x[i] = mul(a[i], fexp(b, i)); x[i] = mul(x[i], fac[i]);
-		y[n - i] = mul(rfac[i], fexp(q, i));
+		x[i] = mul(a[i], fb); x[i] = mul(x[i], fac[i]);
+		y[n - i] = mul(rfac[i], fq);
+		fq = mul(fq, q);
+		fb = mul(fb, b);
 	}
-	conv(n, x, n + 1, y, n + n, h);
+	conv(n, x, n + 1, y, n + n, h);	
 
 	for(i = 0; i < n; i++) p[i + i] = mul(h[i + n], rfac[i]);
 	memset(x, 0, sizeof(x));
 	memset(y, 0, sizeof(y));
+	int fo = 1;
 	for(i = 0; i < m; i++){
 		x[i] = mul(p[i], fac[i]);
-		y[m - i] = mul(fexp(o, i), rfac[i]);
+		y[m - i] = mul(fo, rfac[i]);
+		fo = mul(fo, o);
 	}
 	conv(m, x, m + 1, y, m + m, h);
 	
 	for(i = 0; i < m; i++) p[i] = mul(h[i + m], rfac[i]);
 	memset(x, 0, sizeof(x));
 	memset(y, 0, sizeof(y));
-	for(i = 0; i < m; i++)x[i] = mul(p[i], fexp(c, (LL) i * i));
+	int fc = 1, fcs = fexp(c, i);
+	for(i = 0; i < m; i++){
+		x[i] = mul(p[i], fexp(c, (LL) i * i));
+		//fc = mul(fc, fcs);
+	}
+	// (a + 1) ^ 2 - a ^ 2 = 2a + 1 
 	for(i = 0; i < m + m; i++)
 		y[i] = fexp(c, (LL) - (i - m) * (i - m));
 	conv(m, x, m + m, y, m + n, h);
-	for(i = 0; i < n; i++)printf("%d\n", mul(h[i + m], fexp(c, (LL)i * i)));
+	for(i = 0; i < n; i++)io :: P(mul(h[i + m], fexp(c, (LL)i * i)), '\n');
+	io::output();
 	return 0;
 }
 
