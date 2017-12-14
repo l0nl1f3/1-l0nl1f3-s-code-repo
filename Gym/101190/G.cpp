0eaddf4e9916732f77d9d81fdc056e7eff278ac7
l0nl1f3 <l0nl1f3.gmail.com>
2017-12-14 23:02:10 +0800

    。。。

--- Gym/101190/G.cpp
+++ Gym/101190/G.cpp
@@ -16,7 +16,7 @@ int read() {
 	return x * f;
 }
 
-const int MaxNode = 1001234;
+const int MaxNode = 4001234;
 
 int ch[MaxNode][2], fa[MaxNode], trin;
 string s; int n; vector<int> ed[MaxNode];
@@ -39,10 +39,10 @@ void insert(int a, int b) {
 	e[a].emplace_back(b);
 }
 
-int dfn[MaxNode], low[MaxNode], ind,S[MaxNode], sn, ins[MaxNode], bel[MaxNode], scc;
+int dfn[MaxNode], low[MaxNode], ind, st[MaxNode], sn, ins[MaxNode], bel[MaxNode], scc;
 void dfs(int x) {
 	dfn[x] = low[x] = ++ind;
-	ins[s[++sn] = x] = 1;
+	ins[st[++sn] = x] = 1;
 	for(auto y : e[x]) {
 		if(!dfn[y]) {
 			dfs(y); 
@@ -53,16 +53,20 @@ void dfs(int x) {
 	if(dfn[x] == low[x]) {
 		int v = -1; ++scc;
 		while(sn && v != x) {
-			ins[v = s[sn--]] = 0;
-			bel[x] = ++scc;
+			ins[v = st[sn--]] = 0;
+			bel[v] = scc;
 		}
 	}
 }
 
+string S[501234];
+
 int main() {
+	freopen("binary.in", "r", stdin);
+	freopen("binary.out", "w", stdout);
 	int i, j; n = read();
 	for(i = 1; i <= n; i++) {
-		cin >> s; int pos = -1;
+		cin >> s; S[i] = s; int pos = -1;
 		for(j = 0; j < s.size(); j++) 
 			if(s[j] == '?') {
 				pos = j;
@@ -72,10 +76,10 @@ int main() {
 			s[pos] = '0';
 			append(s, i - 1 << 1);
 			s[pos] = '1';
-			append(s, (i - 1 << 1) - 1);
+			append(s, (i - 1 << 1) + 1);
 		} else {
 			append(s, i - 1 << 1);
-			append(s, (i - 1 << 1) - 1);
+			append(s, (i - 1 << 1) + 1);
 		}
 	}
 	for(i = 1; i <= trin; i++) {
@@ -88,30 +92,41 @@ int main() {
 		#define x ed[i][j]
 		for(j = 0; j < ed[i].size(); j++) {
 			insert(x, i + n * 2);
-			insert(x ^ 1, fa[i] + n * 2);
-			insert(x, trin + 1 + fa[i] * n * 2);
-			insert(trin + 1 * n * 2, x ^ 1);
+			insert(fa[i] + n * 2, x ^ 1);
+			insert(x, trin + 1 + fa[i] + n * 2);
+			insert(trin + 1 + i + n * 2, x ^ 1);
 		}
 		
 		for(j = 0; j < ed[i].size(); j++) {
 			insert(x, ++u);
 			if(j) {
-				insert(j - 1, j);
-				insert(j - 1, x ^ 1);
+				insert(u - 1, u);
+				insert(u - 1, x ^ 1);
 			}
 		}
 		for(j = 0; j < ed[i].size(); j++) {
-			insert(++u, x);
+			insert(++u, x ^ 1);
 			if(j) {
-				insert(j, j - 1);
-				insert(x ^ 1, j - 1);
+				insert(u, u - 1);
+				insert(x, u - 1);
 			}
 		}
 		#undef x
 	}
-	for(i = 1; i <= u; i++) if(!dfn[i]) dfs(i);
-	for(i = 1; i <= n; i++) if(bel[i - 1 << 1] == bel[(i - 1 << 1) - 1]) return puts("No"), 0;
+	for(i = 0; i <= u; i++) if(!dfn[i]) dfs(i);
+	for(i = 1; i <= n; i++) if(bel[i - 1 << 1] == bel[(i - 1 << 1) + 1]) return puts("NO"), 0;
 	puts("YES");
+	for(i = 1; i <= n; i++) {
+		int pos = -1;
+		for(j = 0; j < S[i].size(); j++) 
+			if(S[i][j] == '?') {
+				if(bel[i - 1 << 1] < bel[(i - 1 << 1) + 1])
+					S[i][j] = '0';	
+				else
+					S[i][j] = '1';
+			}
+	}
+	for(i = 1; i <= n; i++) cout << S[i] << endl;
 	return 0;
 }
 
